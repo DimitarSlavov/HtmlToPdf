dca9cbcf0a3a500bdb5b812020d233ff755ebb49
DimSoft <dimsoft@protonmail.com>
2022-06-08 17:14:38 +0300

    Added javascript html to pdf library.

---
+++ NpmJS/src/index.js
@@ -0,0 +1,12 @@
+﻿import { jsPDF } from "jspdf";
+
+window.convertHtmlToPdf = function () {
+    var doc = new jsPDF();
+    var elementHTML = document.getElementById('content');
+
+    doc.html(elementHTML, {
+        'width': 170
+    });
+
+    doc.save('sample-document.pdf');
+}
