8d058edda5e16e57813785813f85517479862d93
DimSoft <dimsoft@protonmail.com>
2022-06-09 16:36:36 +0300

    Optimized pdf generation.

--- NpmJS/src/index.js
+++ NpmJS/src/index.js
@@ -12,16 +12,6 @@ window.convertHtmlToPdf = function () {
         }
     }).then(function ()
     {
-        return arrayBufferToBase64(buffer);
+        return new Uint8Array(buffer);
     });
-}
-
-function arrayBufferToBase64(buffer) {
-	var binary = '';
-    var bytes = new Uint8Array(buffer);
-	var len = bytes.byteLength;
-	for (var i = 0; i < len; i++) {
-		binary += String.fromCharCode(bytes[i]);
-    }
-	return window.btoa(binary);
 }
