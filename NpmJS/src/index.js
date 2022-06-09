7a133eb2a66ba5a5ccf194cbb7552a67c4ce0900
DimSoft <dimsoft@protonmail.com>
2022-06-09 13:55:38 +0300

    In progress.

--- NpmJS/src/index.js
+++ NpmJS/src/index.js
@@ -2,11 +2,23 @@
 
 window.convertHtmlToPdf = function () {
     var doc = new jsPDF();
-    var elementHTML = document.getElementById('content');
+    var elementHTML = document.getElementById('content').value;
+    var parser = new DOMParser;
+    var html = parser.parseFromString(elementHTML, 'text/html');
 
-    doc.html(elementHTML, {
-        'width': 170
-    });
+    doc.html(html);
 
-    doc.save('sample-document.pdf');
+    var buffer = doc.output('arraybuffer');
+
+    return arrayBufferToBase64(buffer);
+}
+
+function arrayBufferToBase64(buffer) {
+	var binary = '';
+	var bytes = new Uint8Array(buffer);
+	var len = bytes.byteLength;
+	for (var i = 0; i < len; i++) {
+		binary += String.fromCharCode(bytes[i]);
+	}
+	return window.btoa(binary);
 }
