c2b2ad556aba4b33cbe95a346f602ea0c8a1cda9
DimSoft <dimsoft@protonmail.com>
2022-06-09 16:32:16 +0300

    Pdf created and returned in the front-end.

--- NpmJS/src/index.js
+++ NpmJS/src/index.js
@@ -3,22 +3,25 @@
 window.convertHtmlToPdf = function () {
     var doc = new jsPDF();
     var elementHTML = document.getElementById('content').value;
-    var parser = new DOMParser;
-    var html = parser.parseFromString(elementHTML, 'text/html');
 
-    doc.html(html);
+    var buffer;
 
-    var buffer = doc.output('arraybuffer');
-
-    return arrayBufferToBase64(buffer);
+    return doc.html(elementHTML, {
+        callback: function (doc) {
+            buffer = doc.output('arraybuffer');
+        }
+    }).then(function ()
+    {
+        return arrayBufferToBase64(buffer);
+    });
 }
 
 function arrayBufferToBase64(buffer) {
 	var binary = '';
-	var bytes = new Uint8Array(buffer);
+    var bytes = new Uint8Array(buffer);
 	var len = bytes.byteLength;
 	for (var i = 0; i < len; i++) {
 		binary += String.fromCharCode(bytes[i]);
-	}
+    }
 	return window.btoa(binary);
 }
